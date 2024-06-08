d02377f2325bd013318d9c96f6a1f70dcc548491
Starkk18 <dubeyswarnim.dubey@gmail.com>
2024-06-08 22:52:45 +0530

    New Branch with update

--- src/app/Service/master.service.spec.ts
+++ src/app/Service/master.service.spec.ts
@@ -5,12 +5,14 @@ import { MasterService } from './master.service';
 describe('MasterService', () => {
   let service: MasterService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MasterService);
-  });
+  
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MasterService);
   });
 });
