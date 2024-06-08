66104d8065e28ef0bf3629d9ee8bd890c29535b9
Starkk18 <dubeyswarnim.dubey@gmail.com>
2024-06-08 23:10:46 +0530

    new component added

---
+++ src/app/ok/ok.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ok',
+  standalone: true,
+  imports: [],
+  templateUrl: './ok.component.html',
+  styleUrl: './ok.component.css'
+})
+export class OkComponent {
+
+}
